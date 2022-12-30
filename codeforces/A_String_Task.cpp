74ba3e4a62189f7d0585cbc0b74983f1df1a1268
Aniket Raj <84118141+aniketraj18@users.noreply.github.com>
2022-12-30 21:46:10 +0530

    Add files via upload

--- codeforces/A_String_Task.cpp
+++ codeforces/A_String_Task.cpp
@@ -1,19 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 void soln(){
-    string s;
-    cin>>s;
-    int n=s.length();
-    string str="";
-    transform(s.begin(), s.end(), s.begin(), ::tolower);
-    for(int i=0;i<n;i++){
-        if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
-        str[i]=' ';
+string s;
+cin>>s;
+string str="";
+for(int i=0;i<s.length();i++){
+    if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'||s[i]=='y'||s[i]=='Y'){
+        str;
+
+    }
+    else{str.push_back('.');
+        if(s[i]>=65&&s[i]<=90)
+        str.push_back(s[i]+32);
         else
-       str[i]=s[i];    
-       }
-       cout<<str<<endl;
-    
+        str.push_back(s[i]);
+    }
+}
+cout<<str<<endl;
 }
 int main(){
     soln();
