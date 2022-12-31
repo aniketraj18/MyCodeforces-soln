36cd1a077e1e4097f4bf426e8c6b7febe993fb91
Aniket Raj <84118141+aniketraj18@users.noreply.github.com>
2023-01-01 01:19:46 +0530

    Add files via upload

--- codeforces/A_Kefa_and_First_Steps.cpp
+++ codeforces/A_Kefa_and_First_Steps.cpp
@@ -3,21 +3,26 @@ using namespace std;
 void soln(){
     int n;
     cin>>n;
-    vector<int>v(n);
+    vector<long long int>v(n);int c=1;int max=INT_MIN;
+    if(n==1){
+        cout<<'1'<<endl;
+    }
+    else{
     for(int i=0;i<n;i++){
         cin>>v[i];
-    }int c=1;
-     for(int i=0;i<n;i++){
-        if(v[i]<=v[i+1]&i+1!=n-1)
+    }
+    for(int i=0;i<n-1;i++){
+        if(v[i+1]>=v[i])
         c++;
         else
         c=1;
-        
+        if(c>max)
+        max=c;
 
-     }
-     cout<<c<<endl;
+    }
+    cout<<max<<endl;}
 }
 int main(){
     soln();
     return 0;
-}
+}//Aniket codes ...
