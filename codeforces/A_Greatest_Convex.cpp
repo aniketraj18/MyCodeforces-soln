5a970d8aae5d2989527fa5a883791f9f7f63106a
Aniket Raj <84118141+aniketraj18@users.noreply.github.com>
2023-01-05 23:12:22 +0530

    Add files via upload

---
+++ codeforces/A_Greatest_Convex.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define l long long
+// int res=0;
+// bool flag=0;
+// l fact(l a){
+//     if(a==0)
+//     return 1;
+//     return fact(a-1);
+// }
+void soln(){
+    l x;
+    cin>>x;
+    cout<<x-1<<endl;
+    // for(l i=x-1;x>=1;x--){
+    //     l ans=fact(i)+fact(i-1);
+    //     if(x%ans==0)
+    //     {
+    //         flag=1;
+    //         res=i;
+    //         break;
+    //     }
+
+    // }
+    // if(flag)
+    // cout<<res<<endl;
+    // else
+    // cout<<"-1"<<endl;
+
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){    soln();}
+    return 0;
+}
