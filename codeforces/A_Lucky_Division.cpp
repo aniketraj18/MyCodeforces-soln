4ffca2e0ab1ddee2a6fdf8f8ac2c4fe5ab53b687
Aniket Raj <84118141+aniketraj18@users.noreply.github.com>
2023-01-12 23:30:50 +0530

    Add files via upload

--- codeforces/A_Lucky_Division.cpp
+++ codeforces/A_Lucky_Division.cpp
@@ -1,25 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool isLucky(int n){
+    int d=0;
+    int num=n;bool flag=true;
+    while(num!=0){
+        d=num%10;
+        num/=10;
+        if(d==4||d==7){
+            continue;
+        
+        }
+        else{
+        flag=false;
+        break;}
+        
+        
+    }
+    if(flag)
+    return flag;
+    else
+    return flag;
+}
 void soln(){
     int n;
     cin>>n;
-    int num=n;bool flag=0;
-     while(num>0){
-        int digit=num%10;
-        if(digit!=4||digit!=7){
-            flag=1;
-            break;
-            
-         }
-         num/=10;
-     }
-     if(flag==1&&!(n%4==0||n%7==0))
-     cout<<"NO"<<endl;
-     if(flag==0&&(n%4==0||n%7==0))
-     cout<<"YES"<<endl;
-
-}
+    if(isLucky(n)){
+        cout<<"YES"<<endl;
+    }
+    else{
+        bool luck=0;
+       for(int i=1;i<=n/2;i++){
+        if(n%i==0){
+        if(isLucky(i)){
+        luck=1;
+        break;
+       }}
+    }
+    if(luck)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+}}
 int main(){
     soln();
-    return 0;
-}
+return 0;}
